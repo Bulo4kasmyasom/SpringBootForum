ab0e2fa292862790015b76c6951eda510bf75a2c
bulo4kasmyasom <benetoru@gmail.com>
2023-05-15 20:00:08 +0300

    now user can be updated without password

--- src/main/java/com/javarush/springbootforum/mapper/UserMapper.java
+++ src/main/java/com/javarush/springbootforum/mapper/UserMapper.java
@@ -6,6 +6,7 @@ import com.javarush.springbootforum.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", uses = {UserMapperBase.class})
 public interface UserMapper {
@@ -18,6 +19,11 @@ public interface UserMapper {
     @Mapping(target = "image", source = "image", qualifiedByName = "setDefaultAvatarImageToUser")
     User toEntity(UserCreateEditDto userCreateEditDto);
 
-    @Mapping(target = "password", source = "password", qualifiedByName = "setEncodingPasswordToUser")
+
+    @Mapping(target = "password", source = "password",
+            conditionExpression = "java(!userCreateEditDto.getPassword().isEmpty())",
+            nullValuePropertyMappingStrategy= NullValuePropertyMappingStrategy.IGNORE,
+            qualifiedByName = "setEncodingPasswordToUser")
+    @Mapping(target = "role", source = "role", qualifiedByName = "setDefaultRoleToUser")
     User toEntity(@MappingTarget User user, UserCreateEditDto userCreateEditDto);
 }
