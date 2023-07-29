a1cc0d5a24c90b2516fe82210b41eb13625d67d3
bulo4kasmyasom <benetoru@gmail.com>
2023-07-29 14:25:26 +0300

    added english translation and added "lang" interceptor

--- src/main/java/com/javarush/springbootforum/config/WebConfiguration.java
+++ src/main/java/com/javarush/springbootforum/config/WebConfiguration.java
@@ -2,9 +2,15 @@ package com.javarush.springbootforum.config;
 
 import com.javarush.springbootforum.interceptor.PageTitleInterceptorHandler;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -15,8 +21,24 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Value("${page.title.afterTitle}")
     private String afterTitle;
 
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
         registry.addInterceptor(new PageTitleInterceptorHandler(beforeTitle, afterTitle)).addPathPatterns("/**");
     }
 
